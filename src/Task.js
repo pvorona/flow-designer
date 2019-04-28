de53617ba92b4ff8014401a0d6cffcc48ea60e3c
Pavel V <vorona.pavel.nickolaevich@gmail.com>
2019-04-28 09:41:28 +0300

    Delete condition by click

--- src/Task.js
+++ src/Task.js
@@ -17,13 +17,15 @@ const iconDotsWidth = 10
 const iconDotsHeight = 16
 const taskHeigh = columnHeight - 2 * vSpacing
 
-export const Task = observer(function Task ({ component: { id, coords: { x, y } } } : Props) {
+export const Task = observer(function Task ({
+  onClick,
+  component: { type, id, coords: { x, y } }
+} : Props) {
   return (
-    <g>
+    <g onClick={onClick}>
       <rect
         style={{
           transform: `translate(calc(50% + ${x + hSpacing - columnWidth / 2}px), ${y + vSpacing}px)`,
-          transition: 'transform .2s ease-in-out',
         }}
         width={columnWidth - 2 * hSpacing}
         height={columnHeight - 2 * vSpacing}
@@ -35,9 +37,10 @@ export const Task = observer(function Task ({ component: { id, coords: { x, y } 
           transform: `translate(calc(50% + ${x - columnWidth / 2 + hSpacing + paddingLeft}px), ${y + vSpacing + fontSize + paddingTop}px)`,
           fill: 'rgba(39, 43, 48, .4)',
           fontSize: 14,
+          userSelect: 'none',
         }}
       >
-        Task
+        {type === 'task' ? 'Task' : 'Condition'} {id}
       </text>
       <text
         style={{
@@ -45,6 +48,7 @@ export const Task = observer(function Task ({ component: { id, coords: { x, y } 
           fontWeight: 'bold',
           fill: '#3B424A',
           fontSize: 14,
+          userSelect: 'none',
         }}
       >
         {['Collect records to process', 'Extract people'][id % 2]}
